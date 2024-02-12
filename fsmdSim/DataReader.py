e1dda71156669291ea0300c5f399098e2d4f5a46
ETHG <skielias@aim.com>
2024-02-12 16:07:11 +0100

    Moved functionality of DependancyContainer.CreateDTOOObjects() to DataReader.readDescriptionData(self). Didn't delete CreateDTOOBjects yet

--- fsmdSim/DataReader.py
+++ fsmdSim/DataReader.py
@@ -1,8 +1,26 @@
 import sys
 import xmltodict
 
+from fsmdSim.DataReader import DataReader
+from model.Condition import Condition
+from model.ConditionsContainer import ConditionsContainer
+from model.Input import Input
 from model.InputStimuliContainer import InputStimuliContainer
 from model.InputStimulus import InputStimulus
+from model.InputsContainer import InputsContainer
+from model.Instruction import Instruction
+from model.InstructionsContainer import InstructionsContainer
+from model.State import State
+from model.StatesContainer import StatesContainer
+from model.Transition import Transition
+from model.TransitionsContainer import TransitionsContainer
+from model.Variable import Variable
+from model.VariablesContainer import VariablesContainer
+from services.InputStimuliExecutor import InputStimuliExecutor
+from services.Logger import Logger
+from services.TransitionsExecutor import TransitionsExecutor
+from services.DynamicExpressionExecutor import DynamicExpressionExecutor
+
 
 
 class DataReader:
@@ -24,6 +42,7 @@ class DataReader:
 
         print("\n--FSMD description--")
 
+        
         states = self.__ReadStates(fsmd_des)
         initialState = self.__ReadInitialState(fsmd_des)
         inputs = self.__ReadInputs(fsmd_des)
@@ -32,7 +51,49 @@ class DataReader:
         conditions = self.__ReadConditions(fsmd_des)
         fsmdTransitions = self.__ReadFsmdTransitions(fsmd_des, states)
 
-        return iterations, initialState, states, inputs, variables, operations, conditions, fsmdTransitions
+
+        stateObjects = {}
+        for state in states:
+            stateObjects[state] = State(state)
+        statesContainer = StatesContainer(stateObjects[initialState])
+        statesContainer.states = stateObjects
+
+        variablesContainer = VariablesContainer()
+        for (name, value) in variables.items():
+            variablesContainer.objects[name] = Variable(name, value)
+
+        inputsContainer = InputsContainer()
+        for (name, value) in inputs.items():
+            inputsContainer.Inputs[name] = Input(name, value)
+
+        instructionsContainer = InstructionsContainer()
+        for (name, value) in operations.items():
+            instructionsContainer.Instructions[name] = Instruction(name, value)
+
+        conditionsContainer = ConditionsContainer()
+        for (name, value) in conditions.items():
+            conditionsContainer.Conditions[name] = Condition(name, value)
+
+        transitionsContainer = TransitionsContainer()
+        for (stateName, rawTransitions) in fsmdTransitions.items():
+            state = statesContainer.states[stateName]
+
+            transitions = []
+            for transition in rawTransitions:
+                nextStateName = transition["nextstate"]
+                conditionName = transition["condition"]
+                instructionsNames = transition["instruction"].strip().split(' ')
+                transitions.append(
+                    Transition(
+                        state,
+                        statesContainer.states[nextStateName],
+                        conditionsContainer.Conditions[conditionName],
+                        [instructionsContainer.Instructions[instructionName] for instructionName in instructionsNames]))
+            transitionsContainer.Transitions[state] = transitions
+
+    
+        return iterations, statesContainer, variablesContainer, inputsContainer, instructionsContainer, conditionsContainer, transitionsContainer, inputStimuliContainer
+
 
     def readStimuliData(self) -> InputStimuliContainer:
         if len(sys.argv) < 3:
